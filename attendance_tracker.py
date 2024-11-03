821ad09a82863516f47e02d3e0b44a84e4baf051
Mango-707 <mirnosirovmirabbos.gmail.com>
2024-11-03 13:58:49 +0500

    adding window for application

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -1,3 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
 import datetime
+
+#Dictionary to store the date
+attendance_records=()
+
+class attendanceapp:
+    def __init__(self, root):
+        self.root=root
+        self.root.titile("Attendance tracker")
+        self.root.geometry("600x400")
+        self.root.configure(bg="white")
+        
