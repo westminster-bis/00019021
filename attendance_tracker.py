2c412211dadfb6570655e3cee2291ddef8d27341
Mirabbos <mirnosirovmirabbos@gmail.com>
2024-11-03 14:02:18 +0500

    adding window

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -1,3 +1,23 @@
 import tkinter as tk
 from tkinter import messagebox
+<<<<<<< HEAD
 import datetime
+
+#Dictionary to store the date
+attendance_records=()
+
+class attendanceapp:
+    def __init__(self, root):
+        self.root=root
+        self.root.titile("Attendance tracker")
+        self.root.geometry("600x400")
+        self.root.configure(bg="white")
+        
+=======
+import datetime 
+
+#Adding dictionary to store the date
+attendance_records = {}
+
+#Class for application
+>>>>>>> a8c35e0ff447ef3d2240c19afd4296425efe413d
