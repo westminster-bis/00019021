f8e656065f24c18305c047ee1a3daa65b0fd1a82
Mirabbos <mirnosirovmirabbos@gmail.com>
2024-11-03 19:14:47 +0500

    Done with front window

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -11,11 +11,19 @@ class attendanceapp:
         root.title("Attendance tracker")
         self.root.geometry("600x400")
         self.root.configure(bg="white")
+
+        #Setting adjustable window
+        center_frame = tk.Frame(self.root, bg="lightgrey")
+        center_frame.place(relx=0.5, rely=0.5, anchor="center")
         
-        #Start button to enter to the menu
-        start_button = tk.Button(root, text="Start attendance tracker", #command=self.open_main_menu,
-                                  bg="white", font=("montserrat", 20, "bold"), padx=20, pady=20)
-        start_button.pack(padx=5, pady=5)
+        #Start button to enter the menu
+        start_button = tk.Button(center_frame, text="Start attendance tracker", #command=self.open_main_menu,
+                                  bg="white", font=("montserrat", 20, "bold"))
+        start_button.grid(row=0, column=0)
+
+        #Window will not be resized smaller then button
+        self.root.update_idletasks()
+        self.root.minsize(start_button.winfo_width() + 50, start_button.winfo_height() + 50)
 
 if __name__ == "__main__":
     root = tk.Tk()
