2ab1d811948900a2867ea5b88758b97c41816df9
Mirabbos <mirnosirovmirabbos@gmail.com>
2024-11-03 15:52:50 +0500

    adding manu

--- attendance_tracker.py
+++ attendance_tracker.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 from tkinter import messagebox
-<<<<<<< HEAD
 import datetime
 
 #Dictionary to store the date
-attendance_records=()
+attendance_records = {}
 
 class attendanceapp:
     def __init__(self, root):
-        self.root=root
-        self.root.titile("Attendance tracker")
+        self.root = root
+        root.title("Attendance tracker")
         self.root.geometry("600x400")
         self.root.configure(bg="white")
         
-=======
-import datetime 
+        #Start button to enter to the menu
+        start_button = tk.Button(root, text="Start attendance tracker", #command=self.open_main_menu,
+                                  bg="white", font=("montserrat", 20, "bold"), padx=20, pady=20)
+        start_button.pack(padx=5, pady=5)
 
-#Adding dictionary to store the date
-attendance_records = {}
-
-#Class for application
->>>>>>> a8c35e0ff447ef3d2240c19afd4296425efe413d
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = attendanceapp(root)
+    root.mainloop()
